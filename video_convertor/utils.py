b38cbc589457c0fa193521f9e38c64ff0f32b5d6
starzkeeper <yaroslav020304@gmail.com>
2023-09-25 19:06:19 +0300

    2 commit

--- video_convertor/utils.py
+++ video_convertor/utils.py
@@ -3,6 +3,6 @@ from moviepy.editor import VideoFileClip
 
 def convert_mp4_to_mp3(mp4_file):
     video = VideoFileClip(f'media/uploads/{mp4_file.mp4_filename()}')
-    mp3_file = video.audio.write_audiofile('media/final/converted.mp3')
+    mp3_file = video.audio.write_audiofile(f'media/final/converted.mp3')
     return mp3_file
 
--- video_convertor/views.py
+++ video_convertor/views.py
@@ -4,6 +4,7 @@ from .forms import UploadFileForm
 from .utils import convert_mp4_to_mp3
 from django.http import FileResponse
 from .models import UploadedFile
+from django.conf import settings
 
 
 def upload_file(request):
@@ -13,8 +14,8 @@ def upload_file(request):
             notdb_mp4_file = request.FILES['file']  # Получение загруженного MP4 файла
             mp4_file = UploadedFile.objects.create(mp4_file=notdb_mp4_file)
             notdb_mp3_file = convert_mp4_to_mp3(mp4_file)  # Конвертация в MP3
-            mp3_file = UploadedFile.objects.create(mp3_file='converted.mp3')
-            return redirect('download_mp3', mp3_filename=f'{mp3_file.mp3_filename()}')
+            mp3_file = UploadedFile.objects.create(mp3_file=f'converted.mp3')
+            return redirect('download_mp3', mp3_filename=mp3_file.mp3_filename())
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
@@ -26,6 +27,11 @@ def download_mp3(request, mp3_filename):
     response = FileResponse(mp3_file)
     response['Content-Type'] = 'audio/mpeg'
     response['Content-Disposition'] = f'attachment; filename="{mp3_filename}"'
-    UploadedFile.objects.all().delete()
-    return response
 
+    os.remove(f'media/final/{mp3_filename}')
+    files = os.listdir('media/uploads/')
+    if files:
+        files = [os.path.join('media/uploads', file) for file in files]
+    for file in files:
+        os.remove(file) # Удаление файлов из папки uploads
+    return response
